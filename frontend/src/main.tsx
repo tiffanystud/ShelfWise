f68ce57b71f65858fb48a4fd02af7a45f64b213e
tiffanystud <tiffanylarsson.stud@gmail.com>
2025-08-18 01:49:40 +0200

    feat: add main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,17 @@
 
 /* frontend/src/main.tsx */
 
-/* Som  <script src="main.js"> */
+/* Som <script src="main.js">, startpunkten */
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/global.css";
 
 
+const htmlContainer = document.getElementById("root");
+const reactApp = createRoot(htmlContainer!);
+
+reactApp.render(<App />);
 
 
